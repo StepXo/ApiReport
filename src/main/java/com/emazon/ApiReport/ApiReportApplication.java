068438e8b23359415321aff3787fc826ceff4710
kariaso <kevinleonardoa2013@gmail.com>
2024-10-13 04:00:43 -0500

    Basic Gradle

---
+++ src/main/java/com/emazon/ApiReport/ApiReportApplication.java
@@ -0,0 +1,13 @@
+package com.emazon.ApiReport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emazon/ApiReport/ApiReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emazon.ApiReport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
